c5bd269d468f4e84d63e76ec563c72435af9ad5d
stefansor <ssorobay00@stuy.edu>
2018-10-09 23:29:35 -0400

    finshed hopefully

---
+++ Point.java
@@ -0,0 +1,28 @@
+public class Point{
+  private double x,y;
+
+  public Point(double X, double Y){
+    x=X;
+    y=Y;
+  }
+
+  public Point(Point p){
+    x= p.x;
+    y= p.y;
+  }
+
+  public double getX(){
+    return x;
+  }
+
+  public double getY(){
+    return y;
+  }
+  public static double distance(Point d, Point s){
+    return Math.sqrt(((s.getX() - d.getX()) * (s.getX() - d.getX())) +
+     (s.getY() - d.getY()) * (s.getY() - d.getY()));
+  }
+  public String toString(){
+    return "(" + this.getX() + "," + this.getY() + ")";
+  }
+}
