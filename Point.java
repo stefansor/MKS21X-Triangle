4a9ea4c3e0e8b0ff79d3c20d958d9223ff47f231
stefansor <ssorobay00@stuy.edu>
2018-10-22 21:12:32 -0400

    modified late to inlcude the distanceTo

--- Point.java
+++ Point.java
@@ -22,6 +22,10 @@ public class Point{
     return Math.sqrt(((s.getX() - d.getX()) * (s.getX() - d.getX())) +
      (s.getY() - d.getY()) * (s.getY() - d.getY()));
   }
+  public double distanceTo(Point other){
+    return Math.sqrt((this.getX() - other.getX()) * (this.getX() - other.getX()) +
+    (this.getY() - other.getY()) * (this.getY() - other.getY()));
+  }
   public String toString(){
     return "(" + this.getX() + "," + this.getY() + ")";
   }
